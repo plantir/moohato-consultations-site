7aeca7874c0013237d87fbe248a9c4afbdb9ed33
Armin <arminkheirkhahan@gmail.com>
2023-12-12 14:38:46 +0330

    change design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -157,7 +157,7 @@
 		<Packages class="mt-10 sm:mt-[180px]" items={data.hair_packages} />
 	</div>
 </div>
-<div class="relative">
+<!-- <div class="relative">
 	<SectionBg />
 	<div class="container absolute top-1/2 -translate-y-1/2 right-1/2 translate-x-1/2">
 		<div class="relative flex flex-col sm:flex-row sm:gap-16 sm:pb-[70px]">
@@ -186,16 +186,16 @@
 			</div>
 		</div>
 	</div>
-</div>
-<div class="container" id="section2">
+</div> -->
+<!-- <div class="container" id="section2">
 	<ProfileBoxSection class="mt-10 sm:-mt-[120px]" />
-</div>
+</div> -->
 <!-- <div class="bg-[#F8EEEEB2] pt-10 pb-[70px] mt-[140px]">
 	<div class="container">
 		<CustomersSection />
 	</div>
 </div> -->
-<div class="relative">
+<!-- <div class="relative">
 	<div class="hidden sm:block absolute -z-10 top-[20px] right-[0px]">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -244,4 +244,4 @@
 	<div class="container mb-10 sm:mb-[210px]" id="sexualPackages">
 		<PackagesSection class="mt-10 sm:mt-[180px]" items={data.sexual_packages} />
 	</div>
-</div>
+</div> -->
